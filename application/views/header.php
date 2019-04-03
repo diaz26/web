117a420bb9145dec5df300792d5aec1b2b5f0864
diaz26 <jeffreydiazaya@outlook.com>
2019-04-02 21:59:04 -0500

    .

--- application/views/header.php
+++ application/views/header.php
@@ -1,15 +1,56 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>MotoPartes - JeffPi</title>
-    <link rel="shortcut icon" href="<?= base_url(); ?>images/moto.png" >
-  	<link rel="icon" href="<?= base_url(); ?>images/moto.png" >
-  </head>
+  <title>MotoPartes - JeffPi</title>
+  <link rel="shortcut icon" href="<?= base_url(); ?>images/moto.png" >
+  <link rel="icon" href="<?= base_url(); ?>images/moto.png" >
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light"  style="background-color: #e3f2fd;">
+    <a class="navbar-brand" href="<?= base_url(); ?>">
+      <img src="<?= base_url(); ?>images/moto.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    <b>JeffPi</b>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto" style="text-align: right">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url(); ?>"><b>Inicio</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url(); ?>index.php/productos"><b>Productos</b></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <b>¿Quienes somos?</b>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: #e3f2fd;">
+            <a class="dropdown-item" href="<?= base_url(); ?>index.php/historia"><b>Historia</b></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="<?= base_url(); ?>index.php/contacto"><b>Contacto</b></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="<?= base_url(); ?>index.php/proveedores"><b>Nuestros proveedores</b></a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Repuestos 100% Garantizados</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+</body>
 </html>
--- application/views/view_home.php
+++ application/views/view_home.php
@@ -1,3 +1,66 @@
 <body>
-  hi
+  <div class="content">
+    <div class="row">
+      <div class="col-lg-7">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="<?= base_url(); ?>images/p2.jpg" class="d-block w-100" width="" height="400" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="<?= base_url(); ?>images/p3.jpg" class="d-block w-100" width="" height="400" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="<?= base_url(); ?>images/p4.jpg" class="d-block w-100" width="" height="400" alt="...">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+          </a>
+        </div>
+      </div><!--col-->
+      <div class="col-lg-5">
+            <div class="card text-center">
+              <div class="card-header" style="padding-bottom: 2px">
+                <h3 class="card-title" style="font-family: hughs; text-align:center; color: #0492E8">Algunos de nuestros productos</h3>
+              </div>
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="card text-white bg-secondary mb-3" style="width: 14rem;">
+                      <img src="<?= base_url(); ?>images/p4.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary btn-sm" style="background-color: #e3f2fd; color: #007BFF">Detalles</a>
+                        <a href="#" class="btn btn-primary btn-sm" style="background-color: #e3f2fd; color: #007BFF">Ver en tienda</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="card text-white bg-secondary mb-3" style="width: 14rem;">
+                      <img src="<?= base_url(); ?>images/p4.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary btn-sm" style="background-color: #e3f2fd; color: #007BFF">Detalles</a>
+                        <a href="#" class="btn btn-primary btn-sm" style="background-color: #e3f2fd; color: #007BFF">Ver en tienda</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+              <div class="card-footer text-muted">
+                <a href="#" class="btn btn-primary" style="background-color: #e3f2fd; color: #007BFF">Ver todos</a>
+              </div>
+        </div>
+      </div>
+    </div><!--row-->
+  </div><!--content-->
 </body>
