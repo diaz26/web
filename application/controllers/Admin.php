060f0289cfab5f0ef21fc5552fb12a84a703fcbe
diaz26 <jeffreydiazaya@outlook.com>
2019-04-13 23:03:52 -0500

    -

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,15 +5,22 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-    if($this->session->userdata('logged_in')){
-			if($this->session->userdata('ROL')=='Teletrabajo'){
-      $this->load->view('header');
-  		$this->load->view('view_admin');
-  		$this->load->view('footer');
+		if($this->session->userdata('logged_in')){
+			if($this->session->userdata('ROL')=='Admin'){
+				$this->load->view('header');
+				$this->load->view('view_admin');
+				$this->load->view('footer');
+			}else {
+				$this->load->view('error_page');
+			}
+		}else {
+			redirect("login");
 		}
-    }else {
-      redirect("login");
-    }
+	}
+
+	public function error()
+	{
+		$this->load->view('error_page');
 	}
 
 }
---
+++ application/views/error_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ERROR 404</title>
+  </head>
+  <body>
+    <div class="content">
+      <img src="<?= base_url(); ?>images/404.jpg" width="100%">
+    </div>
+  </body>
+</html>
